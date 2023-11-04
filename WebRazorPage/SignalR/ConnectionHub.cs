1b50fff5f16dbcd8d00b1a57cdfbc0dbb30b2a7f
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2023-11-05 03:12:13 +0700

    32

--- WebRazorPage/SignalR/ConnectionHub.cs
+++ WebRazorPage/SignalR/ConnectionHub.cs
@@ -58,26 +58,82 @@ namespace WebRazorPage.SignalR
 
         public async Task CreateFeedback(RequestFeedBackRZ feedback)
         {
-            var errors = new InputValidation().ValidateCreateFeedback(feedback);
-            if (errors.HasError())
+            try
             {
-                await Clients.Caller.SendAsync("Error", errors.CreateMessage());
-                return;
-            }    
+                var errors = new InputValidation().ValidateCreateFeedback(feedback);
+                if (errors.HasError())
+                {
+                    await Clients.Caller.SendAsync("Error", errors.CreateMessage());
+                    return;
+                }
 
-            var _feedback = await _feedbackService.CreateFeedBackRz(feedback);
+                var _feedback = await _feedbackService.CreateFeedBackRz(feedback);
 
-            var accounts = await _accountService.SearchAccountRole("MANAGER_OFFICE");
+                var accounts = await _accountService.SearchAccountRole("MANAGER_OFFICE");
 
-            Console.WriteLine(JsonSerializer.Serialize(accounts));
+                Console.WriteLine(JsonSerializer.Serialize(accounts));
 
-            foreach (var account in accounts)
+                foreach (var account in accounts)
+                {
+                    await _notificationService.Create(new Notification()
+                    {
+                        AccountId = account.AccountId,
+                        Message = "321323",
+                        Title = $"An feedback with id {_feedback.FeedBackId} has been created",
+                        NotificationId = Guid.NewGuid(),
+                    });
+
+                    foreach (var connectedClient in ConnectedClients)
+                    {
+                        var connectedAccountId = connectedClient.Value;
+                        var connectedAccount = await _accountService.GetById(connectedAccountId);
+
+                        var role = connectedAccount.Role;
+                        if (role != "MANAGER_OFFICE") continue;
+
+                        var notis = await _notificationService.GetAllNotifications(connectedAccountId);
+                        var _notis = _mapper.Map<List<Notification>, List<NotificationDTO>>(notis);
+
+                        await Clients.Client(connectedClient.Key)
+                            .SendAsync("UpdateNotify", _notis);
+                    }
+                }
+
+                await Clients.Caller.SendAsync("Response", "You have successfully created a feedback");
+            }
+
+            catch (Exception ex)
             {
+                await Clients.Caller.SendAsync("Error", ex.Message);
+            }
+        }
+
+        public async Task CreateFixEquipmentJob(RequestUpdateStatusHistoryRZ job)
+        {
+            try
+            {
+                var errors = new InputValidation().ValidateUpdateStatusHistory(job);
+                if (errors.HasError())
+                {
+                    await Clients.Caller.SendAsync("Error", errors.CreateMessage());
+                    return;
+                }
+
+                var accountId = ConnectedClients[Context.ConnectionId];
+                var account = await _accountService.GetById(accountId);
+                if (account.Role != "MANAGER_OFFICE")
+                {
+                    await Clients.Caller.SendAsync("Error", "You do not have permission to call this function");
+                    return;
+                }
+
+                var _job = await _jobService.UpdateHistoryEquipmentRZ(job);
+
                 await _notificationService.Create(new Notification()
                 {
-                    AccountId = account.AccountId,
+                    AccountId = job.EmployeeId,
                     Message = "321323",
-                    Title = $"An feedback with id {_feedback.FeedBackId} has been created",
+                    Title = $"A fix equipment task with id {_job.TaskId} has been assigned to you",
                     NotificationId = Guid.NewGuid(),
                 });
 
@@ -87,7 +143,7 @@ namespace WebRazorPage.SignalR
                     var connectedAccount = await _accountService.GetById(connectedAccountId);
 
                     var role = connectedAccount.Role;
-                    if (role != "MANAGER_OFFICE") continue;
+                    if (role != "STAFF") continue;
 
                     var notis = await _notificationService.GetAllNotifications(connectedAccountId);
                     var _notis = _mapper.Map<List<Notification>, List<NotificationDTO>>(notis);
@@ -95,145 +151,117 @@ namespace WebRazorPage.SignalR
                     await Clients.Client(connectedClient.Key)
                         .SendAsync("UpdateNotify", _notis);
                 }
+                await Clients.Caller.SendAsync("Response", "You have successfully created a fix equipment task");
             }
-
-
-
-            await Clients.Caller.SendAsync("Response", "You have successfully created a feedback");
-        }
-
-        public async Task CreateFixEquipmentJob(RequestUpdateStatusHistoryRZ job)
-        {
-            var errors = new InputValidation().ValidateUpdateStatusHistory(job);
-            if (errors.HasError())
+            catch (Exception ex)
             {
-                await Clients.Caller.SendAsync("Error", errors.CreateMessage());
-                return;
+                await Clients.Caller.SendAsync("Error", ex.Message);
             }
-
-            var accountId = ConnectedClients[Context.ConnectionId];
-            var account = await _accountService.GetById(accountId);
-            if (account.Role != "MANAGER_OFFICE")
-            {
-                await Clients.Caller.SendAsync("Error", "You do not have permission to call this function");
-                return;
-            }
-
-            var _job = await _jobService.UpdateHistoryEquipmentRZ(job);
-
-            await _notificationService.Create(new Notification()
-            {
-                AccountId = job.EmployeeId,
-                Message = "321323",
-                Title = $"A fix equipment task with id {_job.TaskId} has been assigned to you",
-                NotificationId = Guid.NewGuid(),
-            });
-
-            foreach (var connectedClient in ConnectedClients)
-            {
-                var connectedAccountId = connectedClient.Value;
-                var connectedAccount = await _accountService.GetById(connectedAccountId);
-
-                var role = connectedAccount.Role;
-                if (role != "STAFF") continue;
-
-                var notis = await _notificationService.GetAllNotifications(connectedAccountId);
-                var _notis = _mapper.Map<List<Notification>, List<NotificationDTO>>(notis);
-
-                await Clients.Client(connectedClient.Key)
-                    .SendAsync("UpdateNotify", _notis);
-            }
-            await Clients.Caller.SendAsync("Response", "You have successfully created a fix equipment task");
         }
 
         public async Task CreateEquipmentJob(RequestTaskEquipmentRZ job)
         {
-            var errors = new InputValidation().ValidateTaskEquipment(job);
-            if (errors.HasError())
+            try
             {
-                await Clients.Caller.SendAsync("Error", errors.CreateMessage());
-                return;
+
+                var errors = new InputValidation().ValidateTaskEquipment(job);
+                if (errors.HasError())
+                {
+                    await Clients.Caller.SendAsync("Error", errors.CreateMessage());
+                    return;
+                }
+
+                var accountId = ConnectedClients[Context.ConnectionId];
+                var account = await _accountService.GetById(accountId);
+                if (account.Role != "MANAGER_OFFICE")
+                {
+                    await Clients.Caller.SendAsync("Error", "You do not have permission to call this function");
+                    return;
+                }
+
+                var _job = await _jobService.AddTaskEquipmentByResourceIdRZ(job);
+
+                await _notificationService.Create(new Notification()
+                {
+                    AccountId = job.EmployeeId,
+                    Message = "321323",
+                    Title = $"A create equipment task with id {_job.TaskId} has been assigned to you",
+                    NotificationId = Guid.NewGuid(),
+                });
+
+                foreach (var connectedClient in ConnectedClients)
+                {
+                    var connectedAccountId = connectedClient.Value;
+                    var connectedAccount = await _accountService.GetById(connectedAccountId);
+
+                    var role = connectedAccount.Role;
+                    if (role != "STAFF") continue;
+
+                    var notis = await _notificationService.GetAllNotifications(connectedAccountId);
+                    var _notis = _mapper.Map<List<Notification>, List<NotificationDTO>>(notis);
+
+                    await Clients.Client(connectedClient.Key)
+                        .SendAsync("UpdateNotify", _notis);
+                }
+                await Clients.Caller.SendAsync("Response", "You have successfully created an add equipment task");
             }
-
-            var accountId = ConnectedClients[Context.ConnectionId];
-            var account = await _accountService.GetById(accountId);
-            if (account.Role != "MANAGER_OFFICE")
+            catch (Exception ex)
             {
-                await Clients.Caller.SendAsync("Error", "You do not have permission to call this function");
-                return;
+                await Clients.Caller.SendAsync("Error", ex.Message);
             }
-
-            var _job = await _jobService.AddTaskEquipmentByResourceIdRZ(job);
-
-            await _notificationService.Create(new Notification()
-            {
-                AccountId = job.EmployeeId,
-                Message = "321323",
-                Title = $"A create equipment task with id {_job.TaskId} has been assigned to you",
-                NotificationId = Guid.NewGuid(),
-            });
-
-            foreach (var connectedClient in ConnectedClients)
-            {
-                var connectedAccountId = connectedClient.Value;
-                var connectedAccount = await _accountService.GetById(connectedAccountId);
-
-                var role = connectedAccount.Role;
-                if (role != "STAFF") continue;
-
-                var notis = await _notificationService.GetAllNotifications(connectedAccountId);
-                var _notis = _mapper.Map<List<Notification>, List<NotificationDTO>>(notis);
-
-                await Clients.Client(connectedClient.Key)
-                    .SendAsync("UpdateNotify", _notis);
-            }
-            await Clients.Caller.SendAsync("Response", "You have successfully created an add equipment task");
         }
-
         public async Task CreateResourceJob(RequestTaskResourceRz job)
         {
-            var errors = new InputValidation().ValidateTaskResource(job);
-            if (errors.HasError())
+            try
             {
-                await Clients.Caller.SendAsync("Error", errors.CreateMessage());
-                return;
+                var errors = new InputValidation().ValidateTaskResource(job);
+                if (errors.HasError())
+                {
+                    await Clients.Caller.SendAsync("Error", errors.CreateMessage());
+                    return;
+                }
+
+                Console.WriteLine("ok" + JsonSerializer.Serialize(job));
+                var accountId = ConnectedClients[Context.ConnectionId];
+                var account = await _accountService.GetById(accountId);
+                if (account.Role != "MANAGER_OFFICE")
+                {
+                    await Clients.Caller.SendAsync("Error", "You do not have permission to call this function");
+                    return;
+                }
+
+                var _job = await _jobService.AddTaskResourceRz(job);
+
+                await _notificationService.Create(new Notification()
+                {
+                    AccountId = job.EmployeeId,
+                    Message = "321323",
+                    Title = $"A create resource task with id {_job.TaskId} has been assigned to you",
+                    NotificationId = Guid.NewGuid(),
+                });
+
+                foreach (var connectedClient in ConnectedClients)
+                {
+                    var connectedAccountId = connectedClient.Value;
+                    var connectedAccount = await _accountService.GetById(connectedAccountId);
+
+                    var role = connectedAccount.Role;
+                    if (role != "STAFF") continue;
+
+                    var notis = await _notificationService.GetAllNotifications(connectedAccountId);
+                    var _notis = _mapper.Map<List<Notification>, List<NotificationDTO>>(notis);
+
+                    await Clients.Client(connectedClient.Key)
+                        .SendAsync("UpdateNotify", _notis);
+                }
+                await Clients.Caller.SendAsync("Response", "You have successfully created an add resource task");
             }
-
-            Console.WriteLine("ok" + JsonSerializer.Serialize(job));
-            var accountId = ConnectedClients[Context.ConnectionId];
-            var account = await _accountService.GetById(accountId);
-            if (account.Role != "MANAGER_OFFICE")
+            catch (Exception ex)
             {
-                await Clients.Caller.SendAsync("Error", "You do not have permission to call this function");
-                return;
+                await Clients.Caller.SendAsync("Error", ex.Message);
             }
-
-            var _job = await _jobService.AddTaskResourceRz(job);
-
-            await _notificationService.Create(new Notification()
-            {
-                AccountId = job.EmployeeId,
-                Message = "321323",
-                Title = $"A create resource task with id {_job.TaskId} has been assigned to you",
-                NotificationId = Guid.NewGuid(),
-            });
-
-            foreach (var connectedClient in ConnectedClients)
-            {
-                var connectedAccountId = connectedClient.Value;
-                var connectedAccount = await _accountService.GetById(connectedAccountId);
-
-                var role = connectedAccount.Role;
-                if (role != "STAFF") continue;
-
-                var notis = await _notificationService.GetAllNotifications(connectedAccountId);
-                var _notis = _mapper.Map<List<Notification>, List<NotificationDTO>>(notis);
-
-                await Clients.Client(connectedClient.Key)
-                    .SendAsync("UpdateNotify", _notis);
-            }
-            await Clients.Caller.SendAsync("Response", "You have successfully created an add resource task");
         }
+
     }
 }
 
